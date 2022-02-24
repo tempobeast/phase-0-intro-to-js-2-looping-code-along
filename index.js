d635a46cd87e327714ea4444078bdbca1e966e85
Zach <shaw.zacht@gmail.com>
2022-02-24 12:34:48 -0800

    commit tree 4aa512c49d78ab2d4f5344d36d8d41068be63b07

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const guests = ['Guadalupe', 'Ollie', 'Aki'];
+const messages = [];
+
+function writeCards(guests) {
+    for (let i = 0; i < guests.length; i++) {
+        messages.push(`Thank you, ${guests[i]}, for the wonderful surprise gift!`)
+        //console.log(`Thank you, ${guests[i]}, for the wonderful surprise gift!`)
+    }
+    return messages
+}
+
+function countDown () {
+    for (let i = 10; i > -1; i--) {
+        console.log(i)
+    }
+}
